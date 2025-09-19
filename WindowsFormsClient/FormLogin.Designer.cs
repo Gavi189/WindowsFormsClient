cb49512c153f2e3dac1a1ed19fc2d914fbbf5a16
Seu Nome <seuemail@exemplo.com>
2025-09-19 10:49:22 -0300

    feat(FormLogin): Add labels and pictureBox components in the FormLoginClient.

--- WindowsFormsClient/FormLogin.Designer.cs
+++ WindowsFormsClient/FormLogin.Designer.cs
@@ -30,11 +30,21 @@
         {
             this.pnlAzul = new System.Windows.Forms.Panel();
             this.btnFechar = new System.Windows.Forms.Button();
+            this.pbxLogo = new System.Windows.Forms.PictureBox();
+            this.lblBemVindo = new System.Windows.Forms.Label();
+            this.lblSistema = new System.Windows.Forms.Label();
+            this.lblBiblioteca = new System.Windows.Forms.Label();
+            this.pnlAzul.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlAzul
             // 
             this.pnlAzul.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pnlAzul.Controls.Add(this.lblBiblioteca);
+            this.pnlAzul.Controls.Add(this.lblSistema);
+            this.pnlAzul.Controls.Add(this.lblBemVindo);
+            this.pnlAzul.Controls.Add(this.pbxLogo);
             this.pnlAzul.Location = new System.Drawing.Point(0, 0);
             this.pnlAzul.Name = "pnlAzul";
             this.pnlAzul.Size = new System.Drawing.Size(400, 700);
@@ -56,6 +66,49 @@
             this.btnFechar.UseVisualStyleBackColor = true;
             this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
             // 
+            // pbxLogo
+            // 
+            this.pbxLogo.Image = global::WindowsFormsClient.Properties.Resources.online_library;
+            this.pbxLogo.Location = new System.Drawing.Point(135, 200);
+            this.pbxLogo.Name = "pbxLogo";
+            this.pbxLogo.Size = new System.Drawing.Size(130, 130);
+            this.pbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxLogo.TabIndex = 0;
+            this.pbxLogo.TabStop = false;
+            // 
+            // lblBemVindo
+            // 
+            this.lblBemVindo.AutoSize = true;
+            this.lblBemVindo.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBemVindo.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblBemVindo.Location = new System.Drawing.Point(56, 352);
+            this.lblBemVindo.Name = "lblBemVindo";
+            this.lblBemVindo.Size = new System.Drawing.Size(287, 45);
+            this.lblBemVindo.TabIndex = 1;
+            this.lblBemVindo.Text = "Seja bem-vindo ao";
+            // 
+            // lblSistema
+            // 
+            this.lblSistema.AutoSize = true;
+            this.lblSistema.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSistema.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblSistema.Location = new System.Drawing.Point(42, 397);
+            this.lblSistema.Name = "lblSistema";
+            this.lblSistema.Size = new System.Drawing.Size(321, 45);
+            this.lblSistema.TabIndex = 2;
+            this.lblSistema.Text = "Sistema de Controle";
+            // 
+            // lblBiblioteca
+            // 
+            this.lblBiblioteca.AutoSize = true;
+            this.lblBiblioteca.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBiblioteca.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblBiblioteca.Location = new System.Drawing.Point(98, 442);
+            this.lblBiblioteca.Name = "lblBiblioteca";
+            this.lblBiblioteca.Size = new System.Drawing.Size(197, 45);
+            this.lblBiblioteca.TabIndex = 3;
+            this.lblBiblioteca.Text = "Bibliotecário";
+            // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -67,6 +120,9 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormLogin";
             this.Text = "FormLogin";
+            this.pnlAzul.ResumeLayout(false);
+            this.pnlAzul.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -75,6 +131,10 @@
 
         private System.Windows.Forms.Panel pnlAzul;
         private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.PictureBox pbxLogo;
+        private System.Windows.Forms.Label lblSistema;
+        private System.Windows.Forms.Label lblBemVindo;
+        private System.Windows.Forms.Label lblBiblioteca;
     }
 }
 
